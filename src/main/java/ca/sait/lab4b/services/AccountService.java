98558b9662bb9af1eb9614106babd877b7b25e9b
Jin-young Park <pjy5658@gmail.com>
2022-02-08 20:49:35 -0700

    Lab4B

---
+++ src/main/java/ca/sait/lab4b/services/AccountService.java
@@ -0,0 +1,25 @@
+package ca.sait.lab4b.services;
+
+import ca.sait.lab4b.models.User;
+
+/**
+ *
+ * @author Jin-young Park
+ */
+public class AccountService {
+    public User login(String username, String password) {
+        if(username.equals("abe") && password.equals("password")) {
+            return new User(username, null);
+        }
+        else if(username.equals("barb") && password.equals("password"))
+        {
+            return new User(username, null);
+        }
+        else
+        {
+            return null;
+        }
+        
+    }
+    
+}
